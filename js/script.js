6ee6925b9bf1dfb00938dbde69fc6431cd3d0393
Vanya <engel.stabronik@gmail.com>
2022-02-22 16:14:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result); 
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+/* const answers = [];
+
+ answers[0] = prompt('как ваше имя?', '');
+ answers[1] = prompt('как ваша фамилия?', '');
+ answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null)); */
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Hello, ${user}`); */
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
